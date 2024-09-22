abd7e69a77afb2e2a42bc76193db47169c905eba
Prasanna Narayanan N <155767154+PRASS-NAA@users.noreply.github.com>
2024-09-22 14:20:33 +0530

    Time: 3 ms (36.8%), Space: 7.6 MB (28.59%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        int i = 1;
+        while(1)
+        {
+            if((i%2 == 0) &&(i%n == 0))
+            {
+                return i;
+            }
+            i++;
+        }
+
+        return -1;
+    }
+};
