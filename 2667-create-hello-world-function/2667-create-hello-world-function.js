bb148b902dd52269d70ca6812d4dc8045a167d35
Prasanna Narayanan N <155767154+PRASS-NAA@users.noreply.github.com>
2024-10-07 22:54:38 +0530

    Time: 43 ms (89.87%), Space: 48.9 MB (17.61%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return f;
+    function f()
+    {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
