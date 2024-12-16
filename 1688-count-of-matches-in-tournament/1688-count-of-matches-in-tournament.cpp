4babb9725b2a8adf42e0b816012ec34bdfc8b8b2
Prasanna Narayanan N <155767154+PRASS-NAA@users.noreply.github.com>
2024-12-16 12:15:56 +0530

    Time: 0 ms (100%), Space: 8 MB (9.72%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -3,21 +3,24 @@ public:
     int numberOfMatches(int n) {
         int teams = n;
         int matches = 0;
+        int advteam = 0;
 
         while(teams > 1)
         {
             if(teams %2 == 0)
             {
-                matches += teams/2;
-                //teams = teams-matches;
+                matches = teams/2;
+                advteam += matches;
+                teams = teams-matches;
             }else
             {
-                matches += (teams-1)/2;
-                //teams = teams-matches;
+                matches = ((teams-1)/2)+1;
+                advteam +=  matches;
+                teams = teams-matches;
             }
-            teams-=matches;
+            //teams-=matches;
         }
 
-        return n-1;
+        return advteam;
     }
 };
