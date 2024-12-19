9976e552a31d444165e43a4ab30f782ca5e42db2
Prasanna Narayanan N <155767154+PRASS-NAA@users.noreply.github.com>
2024-12-19 12:55:08 +0530

    Time: 6 ms (57.67%), Space: 17.1 MB (15.89%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long steps = 0;
+        int start;
+        int j = s.length()-1;
+
+        for(int i =0;i<s.length();i++)
+        {
+            if(s[i] == '1')
+            {
+                start = i;
+                break;
+            }
+        }
+
+        while(j>start)
+        {
+           if(s[j] == '0')
+           {
+            swap(s[start],s[j]);
+            steps += j-start;
+            start++;
+            while(s[start] != '1' && start < j)
+            {
+                start++;
+            }
+           }
+           j--;
+        }
+
+        return steps;
+    }
+};
