83e5429c60636def2f7c6c4c3572ed464f993d07
Prasanna Narayanan N <155767154+PRASS-NAA@users.noreply.github.com>
2026-01-20 12:46:36 +0530

    Time: 4 ms (9.65%), Space: 20.2 MB (14.65%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,29 +6,24 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        if not root:
+        self.flag = False
+
+        if root is None:
             return False
 
-        stack = [root]  # Stack stores only nodes
-        sum_stack = [targetSum - root.val]  # Separate sum tracker
+        targetSum -= root.val
 
-        while stack:
-            node = stack.pop()
-            curr_sum = sum_stack.pop()
 
-        # If it's a leaf node and sum matches targetSum
-            if not node.left and not node.right and curr_sum == 0:
-                return True
+        if not self.flag:
+            self.hasPathSum(root.left,targetSum)
+            if not self.flag:
+                self.hasPathSum(root.right, targetSum)
 
-        # Push right child if exists
-            if node.right:
-                stack.append(node.right)
-                sum_stack.append(curr_sum - node.right.val)
+            if root.right is None and root.left is None and not self.flag:
+                if targetSum== 0:
+                    self.flag = True
+                else:
+                    targetSum += root.val    
 
-        # Push left child if exists
-            if node.left:
-                stack.append(node.left)
-                sum_stack.append(curr_sum - node.left.val)
 
-        return False      
-
+        return self.flag                
